34c1fdd664eccc6362c3d42aeedbeb21e5d544a2
Hamad-Khalid <32792523+Hamad-Khalid@users.noreply.github.com>
2017-10-15 15:00:48 +0300

    Commit 3 : 15/10

---
+++ MVCApp/MVCApp/Controllers/HelloController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCApp.Controllers
+{
+    public class HelloController : Controller
+    {
+        //
+        // GET: /Hello/
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Hello from Hamad using MVC";
+            return View();
+        }
+	}
+}
